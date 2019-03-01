458220e94a733a9e89781bbd138d5143f6a28f9b
sunyuecheng <sunyuecheng@sina.cn>
2019-03-01 23:40:04 +0800

    Update install.sh

--- install/kong-install/install.sh
+++ install/kong-install/install.sh
@@ -55,16 +55,15 @@ function check_dir()
 function install_postgresql() {
     yum install ${KONG_POSTGRES_RPM_URL}
 
-    yum install postgresql11-server
+    yum -y install postgresql11 postgresql11-server postgresql11-libs
+
+    mkdir -p /opt/datastore/postgresql/pgsql /mnt/db1/pgdata/pgtbs /mnt/db1/archivelog /backups
+    chmod -R 775 /mnt/db1
+    chown -R postgres:postgres /mnt/db1
 
     /usr/pgsql-11/bin/postgresql-11-setup initdb
     systemctl enable postgresql-11
     systemctl start postgresql-11
-
-groupadd postgres
-useradd -g postgres postgres
-passwd postgres
-
 /etc/rc.d/init.d/postgresql
 }
 
