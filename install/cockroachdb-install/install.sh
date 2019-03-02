7ab1a3d4618f874b88330b1c3b1f0e9989479101
sunyuecheng <sunyuecheng@sina.cn>
2019-03-02 22:43:20 +0800

    Update install script

--- install/cockroachdb-install/install.sh
+++ install/cockroachdb-install/install.sh
@@ -13,6 +13,9 @@ function usage()
     echo ""
     echo "  --package               : package."
     echo "  --install               : install."
+    echo "  --create-root-certs     : create root certs."
+    echo "  --create-node-certs     : create node certs."
+    echo "  --create-client-certs   : create clientcerts."
     echo "  --uninstall             : uninstall."
 }
 
@@ -32,19 +35,6 @@ function check_install()
       return 1
     fi
 
-    install_config_path=${CURRENT_WORK_DIR}/config.properties
-    check_file ${install_config_path}
-    if [ $? != 0 ]; then
-    	echo "Install config ${install_config_path} do not exist."
-      return 1
-    fi
-
-    service_config_path=${CURRENT_WORK_DIR}/config.json
-    check_file ${service_config_path}
-    if [ $? != 0 ]; then
-    	echo "Service config ${service_config_path} do not exist."
-      return 1
-    fi
     return 0
 }
 
@@ -100,83 +90,119 @@ function install()
 
     	echo "Add user group ${SOFTWARE_USER_GROUP} success."
     fi
-    
+
     check_user ${SOFTWARE_USER_NAME}
     if [ $? != 0 ]; then
     	useradd -g ${SOFTWARE_USER_GROUP} -m ${SOFTWARE_USER_NAME}
         usermod -L ${SOFTWARE_USER_NAME}
-      
+
         echo "Add user ${SOFTWARE_USER_NAME} success."
     fi
 
+    father_dir=`dirname ${SOFTWARE_INSTALL_PATH}`
+    mkdir -p ${father_dir}
+    chmod 755 ${father_dir}
     mkdir -p ${SOFTWARE_INSTALL_PATH}
-    chmod u=rwx,g=r,o=r ${SOFTWARE_INSTALL_PATH}
+    chmod 700 ${SOFTWARE_INSTALL_PATH}
+    mkdir -p ${SOFTWARE_INSTALL_PATH}/certs
+    chmod 700 ${SOFTWARE_INSTALL_PATH}/certs
+    mkdir -p ${SOFTWARE_INSTALL_PATH}/safe-dir
+    chmod 700 ${SOFTWARE_INSTALL_PATH}/safe-dir
     chown ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_INSTALL_PATH}
 
+    father_dir=`dirname ${SOFTWARE_DATA_PATH}`
+    mkdir -p ${father_dir}
+    chmod 755 ${father_dir}
     mkdir -p ${SOFTWARE_DATA_PATH}
-    chmod u=rwx,g=r,o=r ${SOFTWARE_DATA_PATH}
-    chown ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_DATA_PATH}
+    chmod 700 ${SOFTWARE_DATA_PATH}
+    mkdir -p ${SOFTWARE_DATA_PATH}/tmp
+    chmod 700 ${SOFTWARE_DATA_PATH}/tmp
+    chown -R ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_DATA_PATH}
 
+    father_dir=`dirname ${SOFTWARE_LOG_PATH}`
+    mkdir -p ${father_dir}
+    chmod 755 ${father_dir}
     mkdir -p ${SOFTWARE_LOG_PATH}
-    chmod u=rwx,g=r,o=r ${SOFTWARE_LOG_PATH}
+    chmod 700 ${SOFTWARE_LOG_PATH}
     chown ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_LOG_PATH}
 
-    package_dir=${CURRENT_WORK_DIR}/${SOFTWARE_SOURCE_PACKAGE_NAME}
-    unzip ${CURRENT_WORK_DIR}/${SOFTWARE_SOURCE_PACKAGE_NAME}
-
-    cp -rf ${CURRENT_WORK_DIR}/${SOFTWARE_INSTALL_PACKAGE_NAME} ${SOFTWARE_INSTALL_PATH}/
-
-    cp -rf ${CURRENT_WORK_DIR}/config.json ${SOFTWARE_INSTALL_PATH}/
+    package_path=${CURRENT_WORK_DIR}/${SOFTWARE_SOURCE_PACKAGE_NAME}
+    tar zxvf ${package_path} -C ${CUR_WORK_DIR}/
+    cp -rf ${CUR_WORK_DIR}/${SOFTWARE_INSTALL_PACKAGE_NAME}/* ${SOFTWARE_INSTALL_PATH}
 
     chown -R ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_INSTALL_PATH}
-    find ${SOFTWARE_INSTALL_PATH} -type d -exec chmod 700 {} \;
     chmod -R u=rwx,g=rwx,o=r ${SOFTWARE_INSTALL_PATH}
 
-    src=SOFTWARE_DATA_PATH
-    dst=${SOFTWARE_DATA_PATH}
-    sed -i "s#$src#$dst#g" ${SOFTWARE_INSTALL_PATH}/config.json
+    return 0
+}
 
-    src=SOFTWARE_LOG_PATH
-    dst=${SOFTWARE_LOG_PATH}
-    sed -i "s#$src#$dst#g" ${SOFTWARE_INSTALL_PATH}/config.json
-
-    src=CONSUL_NODE_NAME
-    dst=${CONSUL_NODE_NAME}
-    sed -i "s#$src#$dst#g" ${SOFTWARE_INSTALL_PATH}/config.json
-
-    src=CONSUL_BIND_IP
-    dst=${CONSUL_BIND_IP}
-    sed -i "s#$src#$dst#g" ${SOFTWARE_INSTALL_PATH}/config.json
-
-    #src=CONSUL_HTTP_PORT
-    #dst=${CONSUL_HTTP_PORT}
-    #sed -i "s#$src#$dst#g" ${SOFTWARE_INSTALL_PATH}/config.json
-
-    src=CONSUL_CLUSTER_CONFIG
-    dst=${CONSUL_CLUSTER_CONFIG}
-    sed -i "s#$src#$dst#g" ${SOFTWARE_INSTALL_PATH}/config.json
-
-    echo "Start to config service ..."
+function config()
+{
     cp ${CURRENT_WORK_DIR}/${SOFTWARE_SERVICE_NAME} /etc/init.d/${SOFTWARE_SERVICE_NAME}
-
-    src=SOFTWARE_INSTALL_PATH
-    dst=${SOFTWARE_INSTALL_PATH}
-    sed -i "s#$src#$dst#g" /etc/init.d/${SOFTWARE_SERVICE_NAME}
 
     src=SOFTWARE_USER_NAME
     dst=${SOFTWARE_USER_NAME}
     sed -i "s#$src#$dst#g" /etc/init.d/${SOFTWARE_SERVICE_NAME}
 
+    src=SOFTWARE_INSTALL_PATH
+    dst=${SOFTWARE_INSTALL_PATH}
+    sed -i "s#$src#$dst#g" /etc/init.d/${SOFTWARE_SERVICE_NAME}
+
+    src=SOFTWARE_LOG_PATH
+    dst=${SOFTWARE_LOG_PATH}
+    sed -i "s#$src#$dst#g" /etc/init.d/${SOFTWARE_SERVICE_NAME}
+
+    src=SOFTWARE_DATA_PATH
+    dst=${SOFTWARE_DATA_PATH}
+    sed -i "s#$src#$dst#g" /etc/init.d/${SOFTWARE_SERVICE_NAME}
+
+    src=COCKROACHDB_MASTER_HOSTS
+    dst=${COCKROACHDB_MASTER_HOSTS}
+    sed -i "s#$src#$dst#g" /etc/init.d/${SOFTWARE_SERVICE_NAME}
+
+    src=COCKROACHDB_HOST
+    dst=${COCKROACHDB_HOST}
+    sed -i "s#$src#$dst#g" /etc/init.d/${SOFTWARE_SERVICE_NAME}
+
+    src=COCKROACHDB_PORT
+    dst=${COCKROACHDB_PORT}
+    sed -i "s#$src#$dst#g" /etc/init.d/${SOFTWARE_SERVICE_NAME}
+
+    src=COCKROACHDB_UI_PORT
+    dst=${COCKROACHDB_UI_PORT}
+    sed -i "s#$src#$dst#g" /etc/init.d/${SOFTWARE_SERVICE_NAME}
+
     chmod 755 /etc/init.d/${SOFTWARE_SERVICE_NAME}
     chkconfig --add ${SOFTWARE_SERVICE_NAME}
 
-    echo "Install success."
+    echo "Install success,
+    please run cmd to create user for ui:
+    cockroach sql --host=localhost:26258
+    CREATE USER user WITH PASSWORD 'pass'; "
+}
 
-    return 0
+function create_root_certs() {
+    ${SOFTWARE_INSTALL_PATH}/cockroach cert create-ca --certs-dir=${SOFTWARE_INSTALL_PATH}/certs --ca-key=${SOFTWARE_INSTALL_PATH}/safe-dir/ca.key
+    #for host in ${COCKROACHDB_NODE_HOST_LIST[@]}; do
+    #    scp ${SOFTWARE_INSTALL_PATH}/certs/ca.crt root@$host:${SOFTWARE_INSTALL_PATH}/certs/ca.crt
+    #    scp ${SOFTWARE_INSTALL_PATH}/safe-dir/ca.key root@$host:${SOFTWARE_INSTALL_PATH}/safe-dir/ca.key
+    #done
+    chown -R ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_INSTALL_PATH}
+}
+
+function create_node_certs() {
+    ${SOFTWARE_INSTALL_PATH}/cockroach cert create-node ${COCKROACHDB_HOST} --certs-dir=${SOFTWARE_INSTALL_PATH}/certs --ca-key=${SOFTWARE_INSTALL_PATH}/safe-dir/ca.key
+    chown -R ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_INSTALL_PATH}
+}
+
+function create_client_certs() {
+    ${SOFTWARE_INSTALL_PATH}/cockroach cert create-client root --certs-dir=${SOFTWARE_INSTALL_PATH}/certs --ca-key=${SOFTWARE_INSTALL_PATH}/safe-dir/ca.key
+    chown -R ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_INSTALL_PATH}
 }
 
 function package()
 {
+
     install_package_path=${CURRENT_WORK_DIR}/${SOFTWARE_SOURCE_PACKAGE_NAME}
     check_file ${install_package_path}
     if [ $? == 0 ]; then
@@ -188,7 +214,7 @@ function package()
         if [ $? == 0 ]; then
             cp -rf ${install_package_path} ./
         else
-            wget https://releases.hashicorp.com/consul/${SOFTWARE_SOURCE_PACKAGE_VERSION}/${SOFTWARE_SOURCE_PACKAGE_NAME}
+            wget https://binaries.cockroachdb.com/${SOFTWARE_SOURCE_PACKAGE_NAME}
         fi
     fi
 }
@@ -203,14 +229,8 @@ function uninstall()
     rm /etc/init.d/${SOFTWARE_SERVICE_NAME}
 
     echo "Uninstall success."
-
     return 0
 }
-
-if [ ! `id -u` = "0" ]; then
-    echo "Please run as root user"
-    exit 1
-fi
 
 if [ $# -eq 0 ]; then
     usage
@@ -226,7 +246,19 @@ elif [ "${opt}" == "--install" ]; then
         echo "Please run as root user"
         exit 2
     fi
+
     install
+    if [ $? != 0 ]; then
+        echo "Install failed,check it please."
+        return 1
+    fi
+    config
+elif [ "${opt}" == "--create-root-certs" ]; then
+    create_root_certs
+elif [ "${opt}" == "--create-node-certs" ]; then
+    create_node_certs
+elif [ "${opt}" == "--create-client-certs" ]; then
+    create_client_certs
 elif [ "${opt}" == "--uninstall" ]; then
     if [ ! `id -u` = "0" ]; then
         echo "Please run as root user"
@@ -238,4 +270,3 @@ elif [ "${opt}" == "--help" ]; then
 else
     echo "Unknown argument"
 fi
-
