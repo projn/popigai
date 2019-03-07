30a516e75dc8ff4e4c2791c89db73764d48da564
projn <projn@sina.com>
2019-03-07 15:24:27 +0800

    Jenkins auto commit alps config server intsall package

--- install/alpsconfigserver-install/install.sh
+++ install/alpsconfigserver-install/install.sh
@@ -117,19 +117,19 @@ function install()
     chown ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_INSTALL_PATH}
 
     mkdir -p ${SOFTWARE_DATA_PATH}
-    chmod u=rw,g=r,o=r ${SOFTWARE_DATA_PATH}
+    chmod u=rwx,g=rx,o=r ${SOFTWARE_DATA_PATH}
     chown ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_DATA_PATH}
 
     mkdir -p ${SOFTWARE_LOG_PATH}
-    chmod u=rw,g=r,o=r ${SOFTWARE_LOG_PATH}
+    chmod u=rwx,g=rx,o=r ${SOFTWARE_LOG_PATH}
     chown ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_LOG_PATH}
 
     package_dir=${CURRENT_WORK_DIR}/${SOFTWARE_INSTALL_PACKAGE_NAME}
     cp -rf ${package_dir}/* ${SOFTWARE_INSTALL_PATH}/
 
     chown -R ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_INSTALL_PATH}
-    chmod u=rwx,g=rwx,o=r  ${SOFTWARE_INSTALL_PATH}/*.jar
-    chmod -R u=rw,g=rw,o=r ${SOFTWARE_INSTALL_PATH}/context/
+    chmod -R u=rwx,g=rx,o=r  ${SOFTWARE_INSTALL_PATH}
+    find ${SOFTWARE_INSTALL_PATH} -type d -exec chmod u=rwx,g=rx,o=r {} \;
 
     echo  "Start to config service ..."
 
