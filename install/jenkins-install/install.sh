69f7bbb7124fb2d14310a6ec439fc8a188ae537a
sunyuecheng <sunyuecheng@sina.cn>
2019-03-07 15:25:05 +0800

    Update install script

--- install/jenkins-install/install.sh
+++ install/jenkins-install/install.sh
@@ -95,32 +95,23 @@ function install()
         echo "Add user ${SOFTWARE_USER_NAME} success."
     fi
 
-    father_dir=`dirname ${SOFTWARE_INSTALL_PATH}`
-    mkdir -p ${father_dir}
-    chmod 755 ${father_dir}
     mkdir -p ${SOFTWARE_INSTALL_PATH}
-    chmod 700 ${SOFTWARE_INSTALL_PATH}
+    chmod u=rwx,g=rx,o=r ${SOFTWARE_INSTALL_PATH}
     chown ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_INSTALL_PATH}
 
-    father_dir=`dirname ${SOFTWARE_DATA_PATH}`
-    mkdir -p ${father_dir}
-    chmod 755 ${father_dir}
     mkdir -p ${SOFTWARE_DATA_PATH}
-    chmod 700 ${SOFTWARE_DATA_PATH}
+    chmod u=rwx,g=rx,o=r ${SOFTWARE_DATA_PATH}
     chown ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_DATA_PATH}
 
-    father_dir=`dirname ${SOFTWARE_LOG_PATH}`
-    mkdir -p ${father_dir}
-    chmod 755 ${father_dir}
     mkdir -p ${SOFTWARE_LOG_PATH}
-    chmod 700 ${SOFTWARE_LOG_PATH}
+    chmod u=rwx,g=r,o=r ${SOFTWARE_LOG_PATH}
     chown ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_LOG_PATH}
 
     package_path=${CURRENT_WORK_DIR}/${SOFTWARE_SOURCE_PACKAGE_NAME}
     cp -rf ${package_path} ${SOFTWARE_INSTALL_PATH}/
 
     chown -R ${SOFTWARE_USER_NAME}:${SOFTWARE_USER_GROUP} ${SOFTWARE_INSTALL_PATH}
-    chmod 755 ${SOFTWARE_INSTALL_PATH}/${SOFTWARE_SOURCE_PACKAGE_NAME}
+    chmod u=rwx,g=rx,o=r ${SOFTWARE_INSTALL_PATH}/${SOFTWARE_SOURCE_PACKAGE_NAME}
 
     yum install -y git
 
